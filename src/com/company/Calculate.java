5eaa8cd8c41863b63c8dd885553a5ad6270aa67f
cookterrence <64807519+cookterrence@users.noreply.github.com>
2020-08-13 12:59:21 -0500

    Update Calculate.java
    Now this class works as it should, it gives both males and females the option of calculating their BMR and Current caloric intake per day and the caloric intake they would need to get to their desired weight

--- src/com/company/Calculate.java
+++ src/com/company/Calculate.java
@@ -1,39 +1,22 @@
 package com.company;
 
+import com.sun.jdi.connect.Connector;
+
 import java.util.Scanner;
 
-public class Calculate{
-    public double BMR;
+public class Calculate {
     private int fat = 3500;
-
-
-    public void male(int currentWeight,double height , int age  ){
-//        BMR = 10W + 6.25H - 5A + 5
-        // Adult male: 66 + (6.3 x body weight in lbs.) +
-        // (12.9 x height in inches) - (6.8 x age in years) = BMR
-        BMR = 10 * currentWeight + 6.25 * height - 5 * age + 5;
-        System.out.println("Your BMR is: " + BMR);
-    }
-
-    public void female(int currentWeight,double height, int age){
-//        BMR = 10W + 6.25H - 5A - 161
-        BMR = 10 * currentWeight + 6.25 * height - 5 * age - 161;
-        System.out.println("Your BMR is: " + BMR);
-    }
-
-
-
-
+    double caloriesPerDay;
 
 
     public void currentcal() {
-        double caloriesPerDay = 0;
+        double BMR = 0;
         Scanner scan = new Scanner(System.in);
         System.out.print("Input Current Weight: ");
         int currentWeight = scan.nextInt();
 
         System.out.print("Input Current height: ");
-        double height = scan.nextInt();
+        int height = scan.nextInt();
 
         System.out.print("Input Current age: ");
         int age = scan.nextInt();
@@ -41,11 +24,17 @@ public class Calculate{
         System.out.println("Are you Male or Female? ");
         String gender = scan.next();
 
-        if(gender.equals("male")){
-            male(currentWeight,height,age);
+        if (gender.equals("male")) {
+//            BMR = 10W + 6.25H - 5A + 5
+            // Adult male: 66 + (6.3 x body weight in lbs.) +
+            // (12.9 x height in inches) - (6.8 x age in years) = BMR
+            BMR = 10 * currentWeight + 6.25 * height - 5 * age + 5;
+            System.out.println("Your BMR is: " + BMR);
 
-        }else if(gender.equals("female")){
-            female(currentWeight, height, age);
+        } else if (gender.equals("female")) {
+            //        BMR = 10W + 6.25H - 5A - 161
+            BMR = 10 * currentWeight + 6.25 * height - 5 * age - 161;
+            System.out.println("Your BMR is: " + BMR);
         }
 
 
@@ -67,26 +56,31 @@ public class Calculate{
 
         int active = scan.nextInt();
 
-        if(active == 1){
+        if (active < 1 || active > 5) {
+            throw new IllegalArgumentException("You have to choose Between 1 and 5");
+        } else if (active == 1) {
             caloriesPerDay = BMR * 1.2;
             System.out.println("Estimated Calories Per Day: " + caloriesPerDay);
 
-        } else if(active == 2){
+        } else if (active == 2) {
             caloriesPerDay = BMR * 1.375;
             System.out.println("Estimated Calories Per Day: " + caloriesPerDay);
 
-        } else if(active == 3){
+        } else if (active == 3) {
             caloriesPerDay = BMR * 1.55;
             System.out.println("Estimated Calories Per Day: " + caloriesPerDay);
 
-        } else if(active == 4){
+        } else if (active == 4) {
             caloriesPerDay = BMR * 1.725;
             System.out.println("Estimated Calories Per Day: " + caloriesPerDay);
 
-        } else {
+        } else if (active == 5) {
             caloriesPerDay = BMR * 1.9;
             System.out.println("Estimated Calories Per Day: " + caloriesPerDay);
+
         }
+
+        // }
 
 
         System.out.print("Would you like to plan out a weight loss program, Enter Yes or No? ");
@@ -102,6 +96,24 @@ public class Calculate{
     }
 
 
+    public void male(int currentWeight, int height, int age) {
+//        BMR = 10W + 6.25H - 5A + 5
+        // Adult male: 66 + (6.3 x body weight in lbs.) +
+        // (12.9 x height in inches) - (6.8 x age in years) = BMR
+        double BMR = 10 * currentWeight + 6.25 * height - 5 * age + 5;
+        System.out.println("Your BMR is: " + BMR);
+        currentcal(BMR);
+    }
+
+
+    public void female(int currentWeight, int height, int age) {
+//        BMR = 10W + 6.25H - 5A - 161
+        double BMR = 10 * currentWeight + 6.25 * height - 5 * age - 161;
+        System.out.println("Your BMR is: " + BMR);
+        currentcal(BMR);
+    }
+
+
     public void currentcal(double caloriesPerDay) {
         int caloriesInFat = 3500;
         System.out.print("How many pounds would you like to lose? ");
@@ -113,29 +125,38 @@ public class Calculate{
         int choice = scan.nextInt();
 
         if (choice == 1) {
-            easyChoice(caloriesPerDay,weightLoss);
+            easyChoice(caloriesPerDay, weightLoss);
         } else {
-            hardChoice(caloriesPerDay,weightLoss);
+            hardChoice(caloriesPerDay, weightLoss);
         }
 
     }
+
+
     public void easyChoice(double caloriesPerDay, int weightloss) {
         System.out.println("This choice will give you the amount of calories you need  to take in to lose  a pound a week");
-        System.out.println("Since you want lose " + weightloss + "pounds you need to cut 500 calories a day to reach that goal in " + weightloss + " weeks" );
+        System.out.println("Since you want lose " + weightloss + "pounds you need to cut 500 calories a day to reach that goal in " + weightloss + " weeks");
         double newcaloriesPerDay = caloriesPerDay - 500;
-        if(newcaloriesPerDay < 1200){
+        if (newcaloriesPerDay < 1200) {
             System.out.println("The suggested minimum amount of calories needed in a day is 1,200 ");
-            System.out.println("Based off your information(BMR) the amount of calories that you would need is below that "+ newcaloriesPerDay + " So we suggest that you lower your caloric intake to 1,200 calories a day.");
+            System.out.println("Based off your information(BMR) the amount of calories that you would need is below that " + newcaloriesPerDay + " So we suggest that you lower your caloric intake to 1,200 calories a day.");
+        } else {
+            System.out.println("This is now your target calories per day: " + newcaloriesPerDay);
         }
     }
-    public void hardChoice(double caloriesPerDay, int weightLoss){
+
+
+    public void hardChoice(double caloriesPerDay, int weightLoss) {
         System.out.println("This choice will give you the amount of calories you need to take in to lose 2 pounds a week");
         System.out.println("Based on health guidelines this is as maximum amount of pounds(2) we suggest  you try to lose a week");
         double newcaloriesPerDay = caloriesPerDay - 1000;
-        if(newcaloriesPerDay < 1200){
+        if (newcaloriesPerDay < 1200) {
             System.out.println("The suggested minimum amount of calories needed in a day is 1,200 ");
-            System.out.println("Based off your information(Calories per day) the amount of calories that you would need is below 1,200 "+ newcaloriesPerDay + " So we suggest that you lower your caloric intake to 1,200 calories a day.");
+            System.out.println("Based off your information(Calories per day) the amount of calories that you would need is below 1,200 " + newcaloriesPerDay + " So we suggest that you lower your caloric intake to 1,200 calories a day.");
+        } else {
+            System.out.println("This is now your target calories per day: " + newcaloriesPerDay);
         }
     }
 }
+
 
