4a4b78845387bf4085c28c793f5a11f6d407e33e
cookterrence <64807519+cookterrence@users.noreply.github.com>
2020-08-17 13:04:18 -0500

    Add a  local database to the application

--- src/com/company/schedule.java
+++ src/com/company/schedule.java
@@ -1,4 +1,12 @@
 package com.company;
 
 public class schedule {
+
+    private int week = 7;
+    private int day = 24;
+
+    public void sleep(){
+        System.out.println("How many hours each day are you available to workout?");
+    }
+
 }
